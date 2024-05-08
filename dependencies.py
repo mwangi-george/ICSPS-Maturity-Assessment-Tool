c825b326cf187453753b120cf0fd5d284c0d6799
mwangi-george <mwangigeorge648@gmail.com>
2024-05-08 07:45:46 +0300

    Updated Instructions

--- dependencies.py
+++ dependencies.py
@@ -19,31 +19,27 @@ project_sections = [
 
 
 purpose = """
-This tool is used to assess the maturity of a country in terms of forecasting and supply planning for vaccines.
-For immunization forecasting and supply planning to be effective, it must be proactive rather than reactive. 
-The tool looks at various characteristics in five broad categories for effective forecasting and supply planning: 
-    
+This tool assesses a country's vaccine forecasting and supply planning maturity. 
+To be effective, immunization forecasting and supply planning must be proactive rather than reactive. 
+The tool looks at various characteristics in five broad categories for effective forecasting and supply planning:
+
     1. FSP Policies, Commitment and Political Will, 
     2. Data, 
     3. Analysis, 
     4. Forecasting, and Supply Planning Activities, 
     5. and Funding and Adjustments of Forecasts and Supply Plans. 
 
-These characteristics holistically contribute to strengthening the forecasting and supply planning practices through the collaborative efforts of all relevant stakeholders in-country thus achieving the desired state of proactive forecasting and supply planning. 
-The assessment results are used to map countries into 3 phases: ad-hoc forecasting and supply planning, reactive forecasting and supply planning, and proactive forecasting and supply planning, with the last being the ideal. 
+These characteristics holistically contribute to strengthening the forecasting and supply planning practices through the collaborative efforts of all relevant stakeholders in-country, thus achieving the desired state of proactive forecasting and supply planning. 
+The assessment results map countries into 3 phases: ad-hoc forecasting and supply planning, reactive forecasting and supply planning, and proactive forecasting and supply planning, with the last being the ideal. 
 Routine monitoring of vaccines by countries ensures that countries maintain adequate stocks of vaccines, align demand for vaccines with supply, and minimize stockouts or the need to destroy vaccines due to expiries.
 """
 
 instructions = """
-The tool is to be completed by country EPI teams that are part of the ICSPS initiative. 
-The assessment will be completed every quarter, to show progress over time. 
-The assessment results will be used by countries to identify and prioritize areas for strengthening, 
-using which the teams will develop action plans for implementation. 
+The tool will be completed by country EPI teams participating in the ICSPS initiative. 
+For each of the questions, please select the answer that best describes the status in the country, particularly the EPI Team, the National Logistics Working Group for Immunization, or any other task force/body in the country that is responsible for supply and demand planning for vaccines within the country.  
 
-For each of the questions, please select the answer that best describes the status in the country, 
-particularly the EPI Team, the National Logistics Working Group for Immunization, or any other task force/body in the country that is responsible for supply and demand planning for vaccines within the country.  
-
-If you have any key comments, please provide them in the sections provided at the bottom of each category in the tool.
+If you have any key comments, please provide them in the sections at the bottom of each category in the tool.
+The assessment will be completed every quarter to show progress over time. The assessment results will help countries pinpoint and prioritize areas needing improvement. Then, teams will use these findings to create action plans for implementation.
 
 """
 default_response_note = "Before you proceed to fill out this digital tool, ensure to first complete a paper-based version. Only input data into the digital tool once the team has collectively agreed upon the responses."
